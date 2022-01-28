9580fa4458f1e5482b97e89ec0357acbf1333aad
pdor <pdor92@hotmail.it>
2022-01-28 09:24:41 +0100

    Added event listener for equals button.

--- script.js
+++ script.js
@@ -54,7 +54,6 @@ clear.addEventListener('click', () => {
 });
 
 // add event listeners for operation buttons
-
 function generateOperatorEventListener(op) {
     return () => {
         operator = op;
@@ -73,3 +72,12 @@ keyDivide.addEventListener('click', generateOperatorEventListener('/'));
 keyMultiply.addEventListener('click', generateOperatorEventListener('*'));
 keySubtract.addEventListener('click', generateOperatorEventListener('-'));
 keyAdd.addEventListener('click', generateOperatorEventListener('+'));
+
+// add event listener for equals button
+const equals = document.querySelector("#key-equals");
+equals.addEventListener('click', () => {
+    const result = operate(operator, operand, number);
+    operand = number;
+    number = result;
+    display.textContent = result;
+});
